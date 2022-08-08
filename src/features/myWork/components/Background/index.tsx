79e6e3ea829a467c98f11377791dc077dc8aa6d2
Serhan Guney <serhan.guney@accolade.com>
2022-08-08 20:41:05 +0200

    feat: create Background

---
+++ src/features/myWork/components/Background/index.tsx
@@ -0,0 +1,115 @@
+import { AnimatePresence } from "framer-motion";
+import Image, { ImageProps } from "next/image";
+import { FC } from "react";
+
+import { durations, easings } from "~/features/core/animations/constants";
+
+import {
+  BackgroundContainer,
+  ImageContainer,
+  ImageWrapper,
+  PrimaryColumn,
+  SecondaryColumn,
+} from "./styles";
+
+type Colors = { primary: string; secondary: string };
+type Props = {
+  duration: number;
+  colors: Colors;
+  images: Array<ImageProps["src"]>;
+};
+
+const imageLayouts = {
+  desktop: {
+    width: "100%",
+    height: "51.5%",
+  },
+  mobile: {
+    width: "46%",
+    height: "100%",
+  },
+};
+
+const Background: FC<Props> = ({ duration, colors, images }) => {
+  const animatePrimaryColumn = {
+    initial: { height: "0%" },
+    animate: { height: "100%", transition: { duration, ease: easings.cubic1 } },
+    exit: { x: 10, opacity: 0, filter: "blur(10px)" },
+  };
+  const animateSecondaryColumn = {
+    initial: { width: "0%" },
+    animate: {
+      width: "80%",
+      transition: {
+        duration,
+        delay: duration - 0.2,
+        ease: easings.cubic1,
+      },
+    },
+    exit: {
+      x: 15,
+      opacity: 0,
+      filter: "blur(10px)",
+      transition: { delay: 0.1 },
+    },
+  };
+  const animateImages = (index: number) => ({
+    initial: {
+      x: -20,
+      opacity: 0,
+      filter: "blur(5px)",
+    },
+    animate: {
+      x: 0,
+      opacity: 1,
+      filter: "blur(0px)",
+    },
+    transition: {
+      type: "spring",
+      stiffness: 250,
+      damping: 15,
+      mass: 1.5,
+      delay: durations.myWork.long + index * 0.2,
+    },
+  });
+
+  return (
+    <BackgroundContainer>
+      <AnimatePresence exitBeforeEnter>
+        <PrimaryColumn
+          key={colors.primary}
+          $color={colors.primary}
+          {...animatePrimaryColumn}
+        />
+        <SecondaryColumn
+          key={colors.secondary}
+          $color={colors.secondary}
+          {...animateSecondaryColumn}
+        />
+
+        <ImageContainer>
+          {images.map((image, index) => {
+            const isMobile = index !== 0;
+            return (
+              <ImageWrapper
+                key={index}
+                $isMobile={isMobile}
+                {...animateImages(index)}
+              >
+                <Image
+                  src={image}
+                  alt=""
+                  objectFit="cover"
+                  layout="responsive"
+                  {...(isMobile ? imageLayouts.mobile : imageLayouts.desktop)}
+                />
+              </ImageWrapper>
+            );
+          })}
+        </ImageContainer>
+      </AnimatePresence>
+    </BackgroundContainer>
+  );
+};
+
+export { Background };
---
+++ src/features/myWork/components/Background/styles.ts
@@ -0,0 +1,50 @@
+import { motion } from "framer-motion";
+import styled from "styled-components";
+
+import { elevations } from "~/features/ui/theme/elevations";
+
+type Color = {
+  $color?: string;
+};
+
+const PrimaryColumn = styled(motion.div)<Color>`
+  ${elevations["0"]};
+  min-width: 20%;
+  background-color: ${(props) => props.$color};
+  align-self: flex-end;
+  position: absolute;
+  left: 0;
+  bottom: 0;
+`;
+const SecondaryColumn = styled(motion.div)<Color>`
+  background-color: ${(props) => props.$color};
+  ${elevations["0"]};
+  position: absolute;
+  left: 20%;
+  top: 0;
+  bottom: 0;
+`;
+const BackgroundContainer = styled.div`
+  grid-column: span 7;
+  position: relative;
+  display: flex;
+`;
+const ImageContainer = styled.div`
+  width: 100%;
+  margin: 4rem 2rem;
+  display: flex;
+  align-items: center;
+`;
+const ImageWrapper = styled(motion.div)<{ $isMobile: boolean }>`
+  ${elevations["900"]};
+  margin: 0 1rem;
+  width: ${(props) => (props.$isMobile ? "14%" : "60%")};
+`;
+
+export {
+  BackgroundContainer,
+  ImageContainer,
+  ImageWrapper,
+  PrimaryColumn,
+  SecondaryColumn,
+};

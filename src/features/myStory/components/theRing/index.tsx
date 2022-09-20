121d0b1494defc792f19e8f3001dc1ef8569e79c
serhanguney <serhan.guney@accolade.com>
2022-09-20 19:41:15 +0200

    feat: animate and style theRing

--- src/features/myStory/components/theRing/index.tsx
+++ src/features/myStory/components/theRing/index.tsx
@@ -1,18 +1,79 @@
-import { FC } from "react";
+import Image from "next/image";
+import { FC, useEffect, useState } from "react";
 
-import { Story } from "~/features/myStory/constants/stories";
+import { easings } from "~/features/core/animations/constants";
+import { stories, Story } from "~/features/myStory/constants/stories";
 
-import { Circle, Container, Image } from "./styles";
+import { Circle, Container, ImageWrapper, Shadow } from "./styles";
 
 type Props = {
-  stories: Story[];
+  story: Story;
 };
 
-const TheRing: FC<Props> = ({ stories }) => {
+const TheRing: FC<Props> = ({ story }) => {
+  const [currentStoryIndex, setCurrentStoryIndex] = useState<number>(
+    stories.indexOf(story)
+  );
+  console.log("TheRing");
+
+  const animateStory = (mappedStory: Story, mappedStoryIndex: number) => {
+    const isCurrentStory = stories.indexOf(mappedStory) === currentStoryIndex;
+    const isSeen = mappedStoryIndex < currentStoryIndex;
+    const isNextOne = mappedStoryIndex - currentStoryIndex === 1;
+    const isLastOne = mappedStoryIndex - currentStoryIndex === 2;
+
+    if (isCurrentStory) return { x: 0, opacity: 1, scale: 1 };
+
+    if (isSeen)
+      return {
+        x: "10%",
+        opacity: 0,
+        filter: `blur(20px)`,
+        transition: { duration: 0.3 },
+      };
+
+    if (isNextOne)
+      return {
+        x: "-100%",
+        scale: 0.7,
+        opacity: 0.5,
+      };
+
+    if (isLastOne)
+      return {
+        x: "-170%",
+        scale: 0.4,
+        opacity: 0.2,
+      };
+  };
+
+  useEffect(() => setCurrentStoryIndex(stories.indexOf(story)), [story]);
+  const storyColor = stories[currentStoryIndex ?? 0].color;
   return (
     <Container>
-      <Circle />
-      <Image />
+      <Circle $borderColor={storyColor}>
+        {stories.map((mappedStory, index) => {
+          const isCurrentStory =
+            stories.indexOf(mappedStory) === currentStoryIndex;
+          const delay = isCurrentStory ? 0 : 0.1;
+          return (
+            <ImageWrapper
+              key={`${mappedStory.title}-index`}
+              $hasFocus={isCurrentStory}
+              initial={animateStory(mappedStory, index)}
+              animate={animateStory(mappedStory, index)}
+              transition={{ duration: 0.6, delay, ease: easings.cubic1 }}
+            >
+              <Image
+                src={mappedStory.coverImage}
+                layout="fill"
+                objectFit="cover"
+              />
+            </ImageWrapper>
+          );
+        })}
+        <Shadow $color={storyColor} />
+      </Circle>
     </Container>
   );
 };
--- src/features/myStory/components/theRing/styles.ts
+++ src/features/myStory/components/theRing/styles.ts
@@ -1,24 +1,49 @@
+import { m } from "framer-motion";
 import styled from "styled-components";
+
+import { colors } from "~/features/ui/theme/colors";
 
 const Container = styled.div`
   display: flex;
-  position: relative;
+  flex-direction: row-reverse;
   grid-column: span 7;
 `;
 
-const Circle = styled.div`
-  border-radius: 40%;
-  border: 1px solid palevioletred;
+const Circle = styled.div<{ $borderColor: string }>`
+  position: relative;
+  flex: 0.4;
+  margin: auto 0;
+  border: 1px solid ${(props) => props.$borderColor};
+  border-radius: 150px;
+  max-height: clamp(34rem, 27vw, 40rem);
+  min-height: 34rem;
+  max-width: 32rem;
+  transition: all 500ms ease;
+`;
+
+const ImageWrapper = styled(m.div)<{ $hasFocus: boolean }>`
   position: absolute;
-  right: 0;
-  left: 60%;
+  left: 0;
   top: 0;
-  bottom: 0;
+  width: 100%;
+  height: 100%;
+  padding: 1.5rem;
+  border-radius: 150px;
+  background-color: ${(props) =>
+    props.$hasFocus ? "antiquewhite" : colors.tag.default};
+  background-clip: content-box;
+  overflow: clip;
+  overflow-clip-margin: content-box;
 `;
 
-const Image = styled.div`
-  background-color: antiquewhite;
-  width: 35%;
+const Shadow = styled.div<{ $color: string }>`
+  position: absolute;
+  bottom: -20%;
+  left: 20%;
+  width: 60%;
+  height: 1rem;
+  filter: blur(20px);
+  background-color: ${(props) => props.$color};
+  transition: background-color 500ms ease;
 `;
-
-export { Circle, Container, Image };
+export { Circle, Container, ImageWrapper, Shadow };

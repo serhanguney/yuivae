37171c68c545e4660a3aa225237a6547b294fdfc
serhanguney <serhan.guney@accolade.com>
2022-10-24 15:01:13 +0200

    feat: finish Header

--- src/features/ui/components/Header/index.tsx
+++ src/features/ui/components/Header/index.tsx
@@ -1,6 +1,6 @@
 import { useElementScroll } from "framer-motion";
 import Link from "next/link";
-import { FC, useEffect, useRef } from "react";
+import { FC, useEffect, useRef, useState } from "react";
 import styled from "styled-components";
 
 import { listOfLinks } from "~/features/core/constants";
@@ -8,6 +8,7 @@ import { colors } from "~/features/ui/theme/colors";
 import { mediaQueries } from "~/features/ui/theme/mediaQueries";
 import { typography } from "~/features/ui/theme/typography";
 import { InvertedLogo } from "~/features/ui/icons/Logo";
+import PreviosArrow from "~/features/ui/icons/PreviosArrow";
 
 const HeaderContainer = styled.header`
   display: flex;
@@ -45,7 +46,7 @@ const LinkContainer = styled.nav`
     align-items: center;
 
     ${mediaQueries.mobileMax} {
-      width: 25rem;
+      width: 27rem;
       height: 5rem;
       white-space: nowrap;
       overflow-x: scroll;
@@ -59,6 +60,9 @@ const LinkContainer = styled.nav`
       flex-shrink: 0;
     }
 
+    &::-webkit-scrollbar {
+      display: none;
+    }
     li {
       ${typography.p};
       font-size: 1.8rem;
@@ -82,11 +86,14 @@ const HeaderLogo = styled(InvertedLogo)`
   height: auto;
 `;
 
-const ScrollCurtain = styled.p`
+const ScrollCurtain = styled.p<{ $isHidden: boolean }>`
   ${mediaQueries.mobileMin} {
     display: none;
   }
   ${typography.p};
+  display: flex;
+  align-items: center;
+  flex-direction: row-reverse;
   flex-shrink: 0;
   width: 100%;
   height: 100%;
@@ -94,17 +101,39 @@ const ScrollCurtain = styled.p`
   text-align: right;
   line-height: 5rem;
   padding-right: 2rem;
+  opacity: ${(props) => (props.$isHidden ? 0 : 1)};
+  transition: opacity 500ms ease-in-out;
+  svg {
+    color: ${colors.text.default};
+    margin-right: 1rem;
+  }
 `;
 const Header: FC = () => {
   const ref = useRef<HTMLUListElement>(null);
   const { scrollX } = useElementScroll(ref);
-  // useEffect(() => scrollX.onChange((latest) => console.log(latest)), []);
+  const [isScrolled, setIsScrolled] = useState<boolean>(false);
+
+  useEffect(
+    () =>
+      scrollX.onChange((latest) => {
+        if (latest > 10) {
+          setIsScrolled(true);
+        } else {
+          setIsScrolled(false);
+        }
+      }),
+    [isScrolled, scrollX]
+  );
+
   return (
     <HeaderContainer>
       <HeaderLogo />
       <LinkContainer>
         <ul ref={ref}>
-          <ScrollCurtain>scroll</ScrollCurtain>
+          <ScrollCurtain $isHidden={isScrolled}>
+            scroll
+            <PreviosArrow isHidden={isScrolled} />
+          </ScrollCurtain>
           {listOfLinks.map((item) => (
             <li key={item.text}>
               <Link href={item.link}>
